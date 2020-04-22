3c21c911c70af99b08b6869c93bf2f1dcd237be0
Miguel219 <34550700+Miguel219@users.noreply.github.com>
2020-04-21 20:21:42 -0600

    c

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -10,23 +10,24 @@ export default function LoginScreen() {
   const [mailInput, changeMailInput] = useState('');
   return (
     <View style={styles.container}>
-        <View style={styles.imageContainer}>
-          <Image
-            source={ require('../assets/images/logoUVGet.png') }
-            style={styles.logoImage}
-          />
-        </View>
-        <View style={styles.formContainer}>
-          <TextInput
-            style={styles.inputContainerStyle}
-            mode={'outlined'}
-            label="Correo"
-            placeholder="Ingresa tu correo"
-            value={mailInput}
-            onChangeText={changeMailInput}
-          />
-        </View>
-        
+      <View style={{flex:0.2}}/>
+      <View style={styles.imageContainer}>
+        <Image
+          source={ require('../assets/images/logoUVGet.png') }
+          style={styles.logoImage}
+        />
+      </View>
+      <View style={{flex:0.1}}/>
+      <View style={styles.formContainer}>
+        <TextInput
+          style={styles.inputContainerStyle}
+          mode={'outlined'}
+          label="Correo"
+          placeholder="Ingresa tu correo"
+          value={mailInput}
+          onChangeText={changeMailInput}
+        />
+      </View>      
     </View>
   );
 }
@@ -34,15 +35,14 @@ export default function LoginScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'column',
     backgroundColor: '#fff',
   },
   inputContainerStyle: {
     margin: 8,
   },
   imageContainer: {
-    alignItems: 'center',
-    marginTop: 120,
-    marginBottom: 50,
+    alignItems: 'center'
   },
   logoImage: {
     width: 120,
