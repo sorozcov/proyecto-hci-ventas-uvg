da50aaf216cfff3b3442f77b06f8bf4aff8f4092
sorozcov <42949349+sorozcov@users.noreply.github.com>
2020-04-26 13:12:48 -0600

    Cambio

--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -9,41 +9,7 @@ import * as firebase from "firebase";
 import 'firebase/firestore';
 import MyTextInput from '../components/textInput';
 
-uriToBlob = (uri) => {
-  return new Promise((resolve, reject) => {
-    const xhr = new XMLHttpRequest();
-    xhr.onload = function() {
-      // return the blob
-      resolve(xhr.response);
-    };
-    
-    xhr.onerror = function() {
-      // something went wrong
-      reject(new Error('uriToBlob failed'));
-    };
-    // this helps us get a blob
-    xhr.responseType = 'blob';
-    xhr.open('GET', uri, true);
-    
-    xhr.send(null);
-  });
-}
 
-
-uploadToFirebase = (blob,uid) => {
-  return new Promise((resolve, reject)=>{
-    let storageRef = firebase.storage().ref();
-    let img = "UserImages/" + uid+'.jpg';
-    storageRef.child(img).put(blob, {
-      contentType: 'image/jpeg'
-    }).then((snapshot)=>{
-      blob.close();
-      resolve(snapshot);
-    }).catch((error)=>{
-      reject(error);
-    });
-  });
-}
 
 async function createUserCollectionFirebase ({email,name,lastName,image,phoneNumber,uid}){
   let db = firebase.firestore();
