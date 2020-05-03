2cd9b36057b7db4fb446eaafbb9111da9cf34b2a
sorozcov <42949349+sorozcov@users.noreply.github.com>
2020-05-03 00:13:07 -0600

    Cambios

--- screens/SalesScreen.js
+++ screens/SalesScreen.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useState} from 'react';
 import { Image, StyleSheet, View ,FlatList} from 'react-native';
 import { connect } from 'react-redux';
 import { withTheme, Text, Button,Card,FAB,Paragraph } from 'react-native-paper';
@@ -9,11 +9,12 @@ import { useScrollToTop } from '@react-navigation/native';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import * as selectors from '../src/reducers';
 import * as actionsMySales from '../src/actions/mySales';
+import SegmentedControlTab from "react-native-segmented-control-tab";
 
-function SalesScreen({ theme, navigation, mySales, selectSale, newSale }) {
+function SalesScreen({ theme, navigation, mySalesNotSold,mySalesSold, selectSale, newSale }) {
   const { colors, roundness } = theme;
   const refFlatList = React.useRef(null);
- 
+  const [indexShowTab, changeIndexShowTab] = useState(0);
   useScrollToTop(refFlatList);
   return (
     <View style={styles.container}>
@@ -38,24 +39,49 @@ function SalesScreen({ theme, navigation, mySales, selectSale, newSale }) {
         onPress={() => newSale(navigation)}>
         NUEVA VENTA
       </Button>
-      { mySales.length==0 &&
+       <SegmentedControlTab
+          values={[ "No vendidos" , "Vendidos"]}
+          activeTabStyle={{backgroundColor:colors.primary}}
+          tabsContainerStyle={{paddingTop:8,marginRight:"10%",marginLeft:"10%"}}
+          tabTextStyle={{fontFamily:'dosis-semi-bold',color:'black'}}
+          tabStyle={{borderColor:'black'}}
+          selectedIndex={indexShowTab}
+          badges={["x","$"]}
+          onTabPress={changeIndexShowTab}
+          tabBadgeContainerStyle={{backgroundColor:'black'}}
+        />
+      { mySalesSold.length==0 && indexShowTab==1 &&
       <View 
         style={styles.containerScrollView} >
         <View style={{...styles.contentContainer, flexDirection: 'column',justifyContent:"center",flex:1}} >
         <MaterialCommunityIcons name="shopify" color={'black'} size={80}
                    style={{textAlign:'center'}} />
         <Text style={styles.textStyle} >
-          Usted no ha creado ninguna venta. </Text>
+          Usted no tiene ninguna venta completada. </Text>
         </View>
         </View>
       
       }
+      { mySalesNotSold.length==0 && indexShowTab==0 &&
+      <View 
+        style={styles.containerScrollView} >
+        <View style={{...styles.contentContainer, flexDirection: 'column',justifyContent:"center",flex:1}} >
+        <MaterialCommunityIcons name="shopify" color={'black'} size={80}
+                   style={{textAlign:'center'}} />
+        <Text style={styles.textStyle} >
+          Usted no tiene ninguna venta pendiente. </Text>
+        </View>
+        </View>
+      
+      }
+      
       <View 
 
         style={styles.containerScrollView} >
         <View style={{...styles.contentContainer, flexDirection: 'column',justifyContent:"space-evenly",flex:1}} >
+        {indexShowTab==0 &&
         <FlatList style={{margin:0}}
-          data={mySales}
+          data={mySalesNotSold}
           ref={refFlatList}
          
           numColumns={2}
@@ -65,14 +91,25 @@ function SalesScreen({ theme, navigation, mySales, selectSale, newSale }) {
             <CardComponent indexShowTab={1} sale={saleItem} onCardClick={() => selectSale(navigation,saleItem.item)} isMySale={true}/>
             )
             
-          }
-          
+          }/>
+          }            
+          {indexShowTab==1 &&
+        <FlatList style={{margin:0}}
+          data={mySalesSold}
+          ref={refFlatList}
+         
+          numColumns={2}
+          keyExtractor={(saleItem, index) => saleItem.saleid}
+          onEndReachedThreshold={0.1}
+          renderItem={(saleItem) => (
+            <CardComponent indexShowTab={1} sale={saleItem} onCardClick={() => selectSale(navigation,saleItem.item)} isMySale={true}/>
+            )
             
-          />
-            
-            
+          }/>
+          }    
             
           </View>
+      
         </View>
     </View>
   );
@@ -108,7 +145,9 @@ const styles = StyleSheet.create({
 
 export default connect(
   state => ({
-    mySales: selectors.getMySales(state),
+
+    mySalesNotSold: selectors.getMySales(state),
+    mySalesSold: selectors.getMySoldSales(state),
   }),
   dispatch => ({
     selectSale(navigation, sale) {
